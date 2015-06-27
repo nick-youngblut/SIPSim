46cb71bb83934072fd41e473a6f9165a38dbdc12
Nick Youngblut <ndy2@cornell.edu>
2015-06-27 14:31:49 -0600

    OTU_taxonAbund: -t and -l

--- lib/R/OTU_taxonAbund.r
+++ lib/R/OTU_taxonAbund.r
@@ -12,8 +12,11 @@ options:
   <OTU>          OTU table file.
   -o=<o>         Base name of output file.
                  [Default: taxonAbund]
+  -t=<t>         File with a list of taxa to select
+                 (1 taxon per line). 
   -r=<r>         Max abundance rank to plot (0 = all).
                  [Default: 0]
+  -l             Include a legend in the plot.
   --width=<w>    Plot width.
                  [Default: 8]
   --height=<h>   Plot height.
@@ -21,7 +24,10 @@ options:
   -h             Help
 
 Description:
-  Plot the abundances of taxa in the OTU table. 
+  Plot the abundances of taxa in the OTU table.
+
+  Filtering:
+    Taxa filtered by rank (-r) prior to the list (-l).
 ' -> doc
 
 opts = docopt(doc)
@@ -55,8 +61,20 @@ calc_BD_mid = function(BD_min, BD_max){
     }
   stop('LOGIC error')
 }
-
 tbl$BD_mid = mapply(calc_BD_mid,tbl$BD_min, tbl$BD_max) 
+
+calc_BD_width = function(BD_min, BD_max){
+  if(is.infinite(BD_min)){
+    return(0.001)
+  } else
+    if(is.infinite(BD_max)){
+      return(0.001)
+    } else {
+      return(BD_max - BD_min)
+    }
+  stop('LOGIC error')
+}
+tbl$BD_width = mapply(calc_BD_width,tbl$BD_min, tbl$BD_max)
 
 
 ## getting relative abundances (counts)
@@ -81,39 +99,50 @@ if(rank.sel > 0){
   tbl = tbl %>%
     filter(taxon %in% tbl.s$taxon)
 }
-
-
+### selecting from taxa from
+if(! is.null(opts[['-t']])){
+  taxa.to.keep = read.table(opts[['t']], header=FALSE, sep='\t')
+  tbl = tbl %>%
+    filter(taxon %in% taxa.to.keep[,1])
+}
 
 ## plotting
 BD.GCp0 = 0 * 0.098 + 1.66
 BD.GCp100 = 1 * 0.098 + 1.66
 
 ## making plots
-make_frac_plot = function(tbl, BD.GCp0, BD.GCp100, alpha=1, rel=FALSE){
+make_frac_plot = function(tbl, BD.GCp0, BD.GCp100, rel=FALSE, legend=FALSE){
   p = ggplot(tbl, aes(x=BD_mid, fill=taxon))
   if(rel==TRUE){
-    p = p + geom_area(aes(y=rel_count), stat='identity', alpha=0.5, position='stack') +
+    p = p + geom_area(aes(y=rel_count), stat='identity', alpha=1, position='stack') +
       labs(y='Relative abundance')
   } else {
     p = p + geom_area(aes(y=count), stat='identity', alpha=0.5, position='dodge') +
       labs(y='Absolute abundance')    
   }  
-  p = p + geom_vline(xintercept=c(BD.GCp0, BD.GCp100), linetype='dashed', alpha=alpha) +
+  p = p + geom_vline(xintercept=c(BD.GCp0, BD.GCp100), linetype='dashed', alpha=0.5) +
       labs(x='Buoyant density') +
         scale_x_continuous(expand=c(0.01,0)) +
           scale_y_continuous(expand=c(0,0.01)) +
             facet_grid(library ~ .) +
-              theme_bw() +
-                theme(
-                  text = element_text(size=16),
-                  legend.position = 'none'
-                )
+              theme_bw()
+
+  if(legend==TRUE){
+    p = p + theme(
+      text = element_text(size=16)
+    )
+  } else {
+    p = p + theme(
+      text = element_text(size=16),
+      legend.position = 'none'
+    )
+  }
+    
   return(p)
 }
 
-
-p.dodge = make_frac_plot(tbl, BD.GCp0, BD.GCp100)
-p.fill = make_frac_plot(tbl, BD.GCp0, BD.GCp100, rel=TRUE)
+p.dodge = make_frac_plot(tbl, BD.GCp0, BD.GCp100, legend=opts[['-l']])
+p.fill = make_frac_plot(tbl, BD.GCp0, BD.GCp100, legend=opts[['-l']], rel=TRUE)
 
 
 ### writing plots
