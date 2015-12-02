1552f8ec79fec68829c820bb2ba154eec98d1370
Nick Youngblut <ndy2@cornell.edu>
2015-12-02 12:31:51 -0700

    added comm_add_richness.r

---
+++ lib/R/comm_add_richness.r
@@ -0,0 +1,128 @@
+#!/usr/bin/Rscript
+
+# init
+rm(list=ls())
+
+# opt parsing
+suppressPackageStartupMessages(library(docopt))
+
+'usage: comm_add_richness.r [options] <comm> <richness>
+
+Options:
+  <comm>      Community file name ("-" if from stdin).
+  <richness>  A table of richness for each community (library).
+  -h          Help
+
+Description:
+  Adding random OTUs (taxa) to community table
+  to reach the richness as specified in the <richness> table.
+  Abundances of new random taxa are obtained from curve-fitting
+  on the abundances in the provided comm file (rel_abund_perc).
+  The random taxa will be appended to the end of the provided
+  community table and written to STDOUT.
+
+  richness table:
+    2-column tab-delimited table: library_id(TAB)richness
+    No header!
+' -> doc
+
+opts = docopt(doc)
+
+
+# packages
+pkgs <- c('dplyr', 'tidyr', 'fitdistrplus')
+for(x in pkgs){
+  suppressPackageStartupMessages(library(x, character.only=TRUE))
+}
+
+# functions
+fit_dist = function(x){
+  x = as.numeric(as.vector(x))
+  # fit
+  f.norm = fitdist(x, 'norm')
+  f.exp = fitdist(x, 'exp')
+  f.logn = fitdist(x, 'lnorm')
+  f.gamma = fitdist(x, 'gamma')
+  
+  f.list = list(f.norm, f.exp, f.logn, f.gamma)
+  names = c('normal', 'exponential', 'lognormal', 'gamma')
+  # best fit (via AIC)
+  stat.res = gofstat(f.list, fitnames=names)
+  max.AIC = which(stat.res$aic == min(stat.res$aic))
+  max.AIC.func = f.list[[max.AIC[1]]]
+  msg = paste0('Best curve fit of abundances: ', names[max.AIC[1]])
+  write(msg, stderr())
+  return(max.AIC.func)
+}
+
+
+get_rel_abunds = function(abund.dist.func, richness){
+  func.l = list(
+    'norm' = rnorm,
+    'exp' = rexp,
+    'lnorm' = rlnorm,
+    'gamma' = rgamma)
+
+  func.name = abund.dist.func$distname
+  func = func.l[[func.name]]
+  abunds = vector()
+  if (func.name == 'exponential'){  # 1 parameter function
+    abunds = func(richness, abund.dist.func$estimate)
+  } else {
+    abunds = func(richness, abund.dist.func$estimate, abund.dist.func$sd)
+  }
+  return(abunds)
+}
+
+make_rand_comm = function(comm.cols, abund.dist.func, lib, richness){
+  # matrix of random taxa
+  m = matrix(ncol=length(comm.cols),
+    nrow=richness,
+    dimnames=list(c(), comm.cols))
+  # lib
+  m[,'library'] = lib
+  # OTU IDs
+  m[,'taxon_name'] = gsub('^', 'rand.', 1:richness)
+  # relative abundances
+  m[,'rel_abund_perc'] = get_rel_abunds(abund.dist.func, richness)
+  m = as.data.frame(m)
+  m$rel_abund_perc = as.numeric(as.vector(m$rel_abund_perc))
+  return(m)
+}
+
+
+
+# main
+## loading comm file
+if(opts[['comm']] == '-'){
+  df.comm = read.delim(file('stdin'), sep='\t')
+} else {
+  df.comm = read.delim(opts[['comm']], sep='\t')
+}
+
+## loading richness table
+df.rich = read.delim(opts[['richness']], sep='\t', header=FALSE)
+
+
+## for each library, adding to richness
+df.comm.cols = colnames(df.comm)
+for (i in 1:nrow(df.rich)){
+  lib = as.character(df.rich[i,1])
+  richness = df.rich[i,2]
+  # curve fitting relative abundance
+  df.comm.p = filter(df.comm, library == lib)
+  abund.dist.func = fit_dist(df.comm.p$rel_abund_perc)
+  
+  # adding random taxa
+  df.rand = make_rand_comm(df.comm.cols, abund.dist.func, lib, richness)
+  df.comm = rbind(df.comm, df.rand)
+}
+
+## adjusting rank
+df.comm = df.comm %>%
+  group_by(library) %>%
+    mutate(rank = row_number(rel_abund_perc))
+
+
+## writing table
+write.table(df.comm, stdout(), quote=FALSE, row.names=FALSE, sep='\t')
